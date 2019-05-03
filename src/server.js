d23299545af339bdc30bf3bd2c6b3aaefe1a8edd
Katie Goldstein <katie.e.goldstein.20@dartmouth.edu>
2019-05-02 23:26:06 -0400

    almost working

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import botkit from 'botkit';
+import yelp from 'yelp-fusion';
+import emoji from 'node-emoji'; // emojissssss
+
+// worked on pseudoCODE with @charlottechui my babe
+
+dotenv.config({ silent: true });
 
 // initialize
 const app = express();
@@ -33,9 +41,145 @@ app.get('/', (req, res) => {
   res.send('hi');
 });
 
-// START THE SERVER
-// =============================================================================
-const port = process.env.PORT || 9090;
-app.listen(port);
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
 
-console.log(`listening on: ${port}`);
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM(err => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) { throw new Error(err); }
+  });
+});
+
+// random direct message 
+controller.on('direct_mention', (bot, message) => {
+  bot.reply(message, 'HI ur yelling. Mo00om I am an adult bot now');
+});
+
+// random dm
+controller.on('direct_message', (bot, message) => {
+  bot.reply(message, 'mmm sliding into those DMs huh');
+});
+
+// rando webhook
+controller.on('outgoing_webhook', (bot, message) => {
+  bot.reply(message, 'Mwah hello.');
+});
+
+ // example hello response
+ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `Howdy there, ${res.user.name}!`);
+    } else {
+      bot.reply(message, 'Heeeellooooo!');
+    }
+  });
+});
+
+// another random message
+controller.hears(['wassup', 'how are you', 'what is up'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `I'm hungry, ${res.user.name}! Are you? Say hungry`);
+    } else {
+      bot.reply(message, 'Tell me you are hungry!!');
+    }
+  });
+})
+
+// help message
+controller.hears('help', ['direct_mention', 'direct_message'], (bot, message) => {
+  bot.reply(message, 'If you type hungry, I will give you food recommendations!!!!! Wooooot');
+});
+
+// Yelp Integration
+const yelpClient = yelp.client(process.env.YELP_CLIENT_SECRET);
+
+controller.hears(['food', 'hungry'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.startConversation(message, (err, convo) => {
+        let location = '';
+        let foodChoice = '';
+
+        convo.addQuestion(`Lol Im just a tweenage but I got u sis, ${res.user.name}. Where u at??? ex like Hanover, NH man`, (response, nextLine) => {
+          location = response.text;
+          nextLine.next();
+        }, { key: 'location' }, 'default');
+
+        convo.addQuestion(`Ok got u bro ${res.user.name}. Now what would you do for a Klondike bar?? JK tell me what food type you want I'm bored. ex sushi`, (response, nextLine) => {
+          foodChoice = response.text;
+
+          if (location.length > 0 && foodChoice.length > 0) {
+            yelpClient.search({
+              term: foodChoice,
+              location,
+            }).then((res) => {
+              convo.say(`Yayyyyyy found some food like this ${foodChoice}: ${res.jsonBody.businesses[0].name}`);
+
+              convo.say(`THis place has like ${res.jsonBody.businesses[0].review_count} reviews on Yelp. Avg rating isssss ${res.jsonBody.businesses[0].rating}. Call ${res.jsonBody.businesses[0].phone} to order ur food now hehe!`);
+
+              const attachments = {
+                attachments: [
+                  {
+                    fallback: 'Yikes what!',
+                    title: res.jsonBody.businesses[0].name,
+                    text: res.jsonBody.businesses[0].url,
+                    color: '#ff0000',
+                    thumb_url: res.jsonBody.businesses[0].image_url,
+                  },
+                ],
+              };
+
+              convo.say(attachments);
+            }).catch((e) => {
+              console.log(e);
+            });
+          }
+
+          nextLine.next();
+        }, { key: 'food' }, 'default');
+
+        convo.addMessage('Oops sry I am WORKINGGGG lo siento', 'default');
+      });
+    } else {
+      bot.reply(message, 'Lol rip re do this plsszzzz!');
+    }
+  });
+});
+
+// if they say anything else that isn't predefined
+controller.hears('^[A-z]+$', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.reply(message, 'Lol hiiiii I do nawwt understand u rn so just come again??');
+});
+
+
+// wake my bot UPpppp
+controller.on('outgoing_webhook', (bot, message) => {
+  bot.replyPublic(message, 'Hiiiiiii u rang??');
+});
+
+
+
+
+
+// commenting out the server!
+// START THE SERVER
+// // =============================================================================
+// const port = process.env.PORT || 9090;
+// app.listen(port);
+
+// console.log(`listening on: ${port}`);
